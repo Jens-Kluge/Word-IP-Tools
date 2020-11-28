1d7b90d630ccb113ab553c73f7735d8f0fa3cff5
Jens-Kluge <jklu@arcor.de>
2020-11-28 20:42:19 +0100

    Add project files.

---
+++ iptools/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmNav
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmNav))
+        Me.btnNumberPars = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.btnRemoveThis = New System.Windows.Forms.Button()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.btnFwd = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.btnRewind = New System.Windows.Forms.Button()
+        Me.btnffwd = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.lblDigits = New System.Windows.Forms.Label()
+        Me.lblPreview = New System.Windows.Forms.Label()
+        Me.btnAddNumberSel = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnNumberPars
+        '
+        Me.btnNumberPars.Location = New System.Drawing.Point(70, 30)
+        Me.btnNumberPars.Name = "btnNumberPars"
+        Me.btnNumberPars.Size = New System.Drawing.Size(154, 56)
+        Me.btnNumberPars.TabIndex = 0
+        Me.btnNumberPars.Text = "Number Paragraphs"
+        Me.btnNumberPars.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(402, 30)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(154, 56)
+        Me.btnUpdate.TabIndex = 1
+        Me.btnUpdate.Text = "Update Fields"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(230, 30)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(166, 56)
+        Me.btnRemove.TabIndex = 2
+        Me.btnRemove.Text = "Remove all paragraph numbers"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'btnRemoveThis
+        '
+        Me.btnRemoveThis.Location = New System.Drawing.Point(230, 92)
+        Me.btnRemoveThis.Name = "btnRemoveThis"
+        Me.btnRemoveThis.Size = New System.Drawing.Size(166, 56)
+        Me.btnRemoveThis.TabIndex = 3
+        Me.btnRemoveThis.Text = "Remove numbering in selected paragraphs"
+        Me.btnRemoveThis.UseVisualStyleBackColor = True
+        '
+        'btnBack
+        '
+        Me.btnBack.Image = Global.iptools.My.Resources.Resources.icons8_back_100
+        Me.btnBack.Location = New System.Drawing.Point(129, 13)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(87, 83)
+        Me.btnBack.TabIndex = 4
+        Me.ToolTip1.SetToolTip(Me.btnBack, "Move back to previous paragraph.")
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'btnFwd
+        '
+        Me.btnFwd.Image = Global.iptools.My.Resources.Resources.icons8_play_100
+        Me.btnFwd.Location = New System.Drawing.Point(232, 13)
+        Me.btnFwd.Name = "btnFwd"
+        Me.btnFwd.Size = New System.Drawing.Size(87, 83)
+        Me.btnFwd.TabIndex = 5
+        Me.ToolTip1.SetToolTip(Me.btnFwd, "Move forward to next paragraph.")
+        Me.btnFwd.UseVisualStyleBackColor = True
+        '
+        'btnRewind
+        '
+        Me.btnRewind.Image = Global.iptools.My.Resources.Resources.icons8_rewind_100
+        Me.btnRewind.Location = New System.Drawing.Point(21, 13)
+        Me.btnRewind.Name = "btnRewind"
+        Me.btnRewind.Size = New System.Drawing.Size(92, 83)
+        Me.btnRewind.TabIndex = 6
+        Me.ToolTip1.SetToolTip(Me.btnRewind, "Move back 5 paragraphs.")
+        Me.btnRewind.UseVisualStyleBackColor = True
+        '
+        'btnffwd
+        '
+        Me.btnffwd.Image = Global.iptools.My.Resources.Resources.icons8_fast_forward_100
+        Me.btnffwd.Location = New System.Drawing.Point(335, 13)
+        Me.btnffwd.Name = "btnffwd"
+        Me.btnffwd.Size = New System.Drawing.Size(92, 83)
+        Me.btnffwd.TabIndex = 7
+        Me.ToolTip1.SetToolTip(Me.btnffwd, "Move forward 5 paragraphs.")
+        Me.btnffwd.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnffwd)
+        Me.Panel1.Controls.Add(Me.btnFwd)
+        Me.Panel1.Controls.Add(Me.btnRewind)
+        Me.Panel1.Controls.Add(Me.btnBack)
+        Me.Panel1.Location = New System.Drawing.Point(49, 160)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(438, 107)
+        Me.Panel1.TabIndex = 7
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(514, 184)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {5, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(47, 22)
+        Me.NumericUpDown1.TabIndex = 8
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {4, 0, 0, 0})
+        '
+        'lblDigits
+        '
+        Me.lblDigits.AutoSize = True
+        Me.lblDigits.Location = New System.Drawing.Point(513, 160)
+        Me.lblDigits.Name = "lblDigits"
+        Me.lblDigits.Size = New System.Drawing.Size(43, 17)
+        Me.lblDigits.TabIndex = 9
+        Me.lblDigits.Text = "Digits"
+        '
+        'lblPreview
+        '
+        Me.lblPreview.AutoSize = True
+        Me.lblPreview.Location = New System.Drawing.Point(513, 220)
+        Me.lblPreview.Name = "lblPreview"
+        Me.lblPreview.Size = New System.Drawing.Size(48, 17)
+        Me.lblPreview.TabIndex = 10
+        Me.lblPreview.Text = "[0001]"
+        '
+        'btnAddNumberSel
+        '
+        Me.btnAddNumberSel.Location = New System.Drawing.Point(70, 92)
+        Me.btnAddNumberSel.Name = "btnAddNumberSel"
+        Me.btnAddNumberSel.Size = New System.Drawing.Size(154, 56)
+        Me.btnAddNumberSel.TabIndex = 11
+        Me.btnAddNumberSel.Text = "Add numbering in selected paragraphs"
+        Me.btnAddNumberSel.UseVisualStyleBackColor = True
+        '
+        'frmNav
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(606, 279)
+        Me.Controls.Add(Me.btnAddNumberSel)
+        Me.Controls.Add(Me.lblPreview)
+        Me.Controls.Add(Me.lblDigits)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.btnRemoveThis)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnNumberPars)
+        Me.Controls.Add(Me.Panel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(342, 186)
+        Me.Name = "frmNav"
+        Me.Text = "Number Paragraphs"
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnNumberPars As Windows.Forms.Button
+    Friend WithEvents btnUpdate As Windows.Forms.Button
+    Friend WithEvents btnRemove As Windows.Forms.Button
+    Friend WithEvents btnRemoveThis As Windows.Forms.Button
+    Friend WithEvents btnBack As Windows.Forms.Button
+    Friend WithEvents btnFwd As Windows.Forms.Button
+    Friend WithEvents ToolTip1 As Windows.Forms.ToolTip
+    Friend WithEvents btnRewind As Windows.Forms.Button
+    Friend WithEvents Panel1 As Windows.Forms.Panel
+    Friend WithEvents btnffwd As Windows.Forms.Button
+    Friend WithEvents NumericUpDown1 As Windows.Forms.NumericUpDown
+    Friend WithEvents lblDigits As Windows.Forms.Label
+    Friend WithEvents lblPreview As Windows.Forms.Label
+    Friend WithEvents btnAddNumberSel As Windows.Forms.Button
+End Class
---
+++ iptools/Form1.vb
@@ -0,0 +1,288 @@
+﻿Imports System.Text.RegularExpressions
+Public Class frmNav
+    'The paragraphs in the document are numbered with SEQ fields, 
+    'the SEQ list has the identifier IPT_PARS
+    'the format is [0001], wherein the number of leading zeros is determined by the updown control
+    Private Sub btnNumberPars_Click(sender As Object, e As EventArgs) Handles btnNumberPars.Click
+        Dim doc As Word.Document
+        Dim dos As Word.Documents
+        Dim par As Word.Paragraph
+        Dim partext, strFormat As String
+        Dim rg As Word.Range
+        Dim sel As Word.Selection
+        Dim fld As Word.Field
+
+        On Error GoTo errHandler
+        'number paragraphs in the active document
+
+        'no docuemnt open => exit this subroutien
+        If Globals.ThisAddIn.Application.Documents.Count = 0 Then
+            Exit Sub
+        End If
+        doc = Globals.ThisAddIn.Application.ActiveDocument
+
+        strFormat = genFormatString()
+
+        Globals.ThisAddIn.Application.ScreenUpdating = False
+
+        For Each par In doc.Paragraphs
+
+            rg = par.Range
+            partext = rg.Text
+
+            If IsParagraph(par) Then
+
+                rg.Collapse(Direction:=Word.WdCollapseDirection.wdCollapseStart)
+                fld = rg.Fields.Add(rg, Word.WdFieldType.wdFieldEmpty, "SEQ IPT_PARS \# " & strFormat, True)
+
+            End If
+
+        Next
+
+        Globals.ThisAddIn.Application.ScreenUpdating = True
+        Exit Sub
+errHandler:
+        'MsgBox(Err.Description, MsgBoxStyle.Critical)
+        Globals.ThisAddIn.Application.ScreenUpdating = True
+        If Not Information.IsNothing(par) Then
+            Resume Next
+        End If
+    End Sub
+
+    Function IsParagraph(par As Word.Paragraph)
+
+        Dim parlen As Integer
+        Dim text As String
+
+        IsParagraph = True
+        text = par.Range.Text
+        'remove non-printable characters
+        text = Regex.Replace(Text, "[^A-Za-z0-9\-/]", "")
+        parlen = Len(Text)
+        If parlen = 0 Then IsParagraph = False
+
+    End Function
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim doc As Word.Document
+        'update the fields
+
+        'no docuemnt open => exit this subroutien
+        If Globals.ThisAddIn.Application.Documents.Count = 0 Then
+            Exit Sub
+        End If
+        doc = Globals.ThisAddIn.Application.ActiveDocument
+
+        doc.Fields.Update()
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        Dim doc As Word.Document
+        Dim fld As Word.Field
+        Dim txt As String
+
+        'no docuemnt open => exit this subroutien
+        If Globals.ThisAddIn.Application.Documents.Count = 0 Then
+            Exit Sub
+        End If
+
+        doc = Globals.ThisAddIn.Application.ActiveDocument
+
+        Globals.ThisAddIn.Application.ScreenUpdating = False
+        For Each fld In doc.Fields
+            If Not Information.IsNothing(fld.Code) Then
+                txt = fld.Code.Text
+                If InStr(txt, "PARS") > 0 Then
+                    fld.Delete()
+                End If
+            End If
+
+        Next
+        Globals.ThisAddIn.Application.ScreenUpdating = True
+    End Sub
+
+    Private Sub frmNav_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnRemoveThis_Click(sender As Object, e As EventArgs) Handles btnRemoveThis.Click
+
+        Dim sel As Word.Selection
+        Dim par As Word.Paragraph
+        Dim fld As Word.Field
+        Dim txt As String
+
+        sel = Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection
+        For Each par In sel.Paragraphs
+
+            For Each fld In par.Range.Fields
+                If Not Information.IsNothing(fld.Code) Then
+                    txt = fld.Code.Text
+                    If InStr(txt, "PARS") > 0 Then
+                        fld.Delete()
+                    End If
+                End If
+            Next
+        Next
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        Dim par As Word.Paragraph
+        Dim sel As Word.Selection
+        On Error GoTo ErrHandler
+
+        If Information.IsNothing(Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection) Then
+            Exit Sub
+        End If
+
+        sel = Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection
+        par = sel.Paragraphs(sel.Paragraphs.Count)
+
+        If Information.IsNothing(par.Next) Then
+            Exit Sub
+        End If
+        par = par.Previous
+        par.Range.Select()
+        Exit Sub
+
+ErrHandler:
+        MsgBox(Err.Description)
+    End Sub
+
+    Private Sub btnFwd_Click(sender As Object, e As EventArgs) Handles btnFwd.Click
+        Dim par As Word.Paragraph
+        Dim sel As Word.Selection
+        On Error GoTo errHandler
+
+        If Information.IsNothing(Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection) Then
+            Exit Sub
+        End If
+
+        sel = Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection
+        par = sel.Paragraphs(sel.Paragraphs.Count)
+
+        If Information.IsNothing(par.Next) Then
+            Exit Sub
+        End If
+        par = par.Next
+        par.Range.Select()
+        Exit Sub
+ErrHandler:
+        MsgBox(Err.Description)
+    End Sub
+
+    Private Sub btnRewind_Click(sender As Object, e As EventArgs) Handles btnRewind.Click
+        Dim sel As Word.Selection
+        Dim par, prevPar As Word.Paragraph
+        On Error GoTo ErrHandler
+
+        If Information.IsNothing(Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection) Then
+            Exit Sub
+        End If
+
+        sel = Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection
+        par = sel.Paragraphs(1)
+
+        For i = 1 To 5
+
+
+            If Information.IsNothing(par.Previous) Then
+                Exit Sub
+            End If
+
+            par = par.Previous
+            par.Range.Select()
+        Next
+
+        Exit Sub
+ErrHandler:
+        MsgBox(Err.Description)
+    End Sub
+
+    Private Sub btnffwd_Click(sender As Object, e As EventArgs) Handles btnffwd.Click
+        Dim sel As Word.Selection
+        Dim par As Word.Paragraph
+        On Error GoTo ErrHandler
+
+        If Information.IsNothing(Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection) Then
+            Exit Sub
+        End If
+
+        sel = Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection
+        par = sel.Paragraphs(sel.Paragraphs.Count)
+
+        For i = 1 To 5
+
+
+            If Information.IsNothing(par.Next) Then
+                Exit Sub
+            End If
+
+            par = par.Next
+            par.Range.Select()
+        Next
+
+        Exit Sub
+ErrHandler:
+        MsgBox(Err.Description)
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        Dim strdisp As String
+        strdisp = "00001"
+        strdisp = strdisp.Substring(strdisp.Length - NumericUpDown1.Value, NumericUpDown1.Value)
+        strdisp = "[" & strdisp & "]"
+
+        lblPreview.Text = strdisp
+    End Sub
+
+    Private Function genFormatString() As String
+
+        Dim strformat As String
+        strformat = "00000"
+        strFormat = strFormat.Substring(strFormat.Length - NumericUpDown1.Value, NumericUpDown1.Value)
+        strFormat = Chr(34) & "[" & strFormat & "] " & Chr(34)
+
+        genFormatString = strformat
+
+    End Function
+
+    Private Sub btnAddNumberSel_Click(sender As Object, e As EventArgs) Handles btnAddNumberSel.Click
+
+        Dim rg As Word.Range
+        Dim sel As Word.Selection
+        Dim par As Word.Paragraph
+        Dim fld As Word.Field
+        Dim partext, strformat As String
+        Dim pars As Word.Paragraphs
+
+
+        On Error GoTo errHandler
+
+        If Information.IsNothing(Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection) Then
+            Exit Sub
+        End If
+
+        strformat = genFormatString()
+
+        sel = Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection
+        pars = sel.Paragraphs
+
+        For Each par In pars
+
+            rg = par.Range
+            partext = rg.Text
+
+            If IsParagraph(par) Then
+
+                rg.Collapse(Direction:=Word.WdCollapseDirection.wdCollapseStart)
+                fld = rg.Fields.Add(rg, Word.WdFieldType.wdFieldEmpty, "SEQ IPT_PARS \# " & strformat, True)
+
+            End If
+
+        Next
+        Exit Sub
+errHandler:
+        MsgBox(Err.Description)
+    End Sub
+End Class
